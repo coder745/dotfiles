1f1b59998fa53a665d712ba36b610442eb9050e9
Josh Young <josh.young@daveramsey.com>
2020-10-20 11:03:06 -0500

    Add script for install VS Code plugins

---
+++ install_vs_code_plugins.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+code --install-extension formulahendry.auto-close-tag
+code --install-extension aaron-bond.better-comments
+code --install-extension alefragnani.bookmarks
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension naumovs.color-highlight
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension editorconfig.editorconfig
+code --install-extension vincaslt.highlight-matching-tag
+code --install-extension sirtori.indenticator
+code --install-extension orta.vscode-jest
+code --install-extension firsttris.vscode-jest-runner
+
+# One Dark Pro
+code --install-extension zhuangtongfa.material-theme
+code --install-extension ibm.output-colorizer
+code --install-extension esbenp.prettier-vscode
+code --install-extension noku.rails-run-spec-vscode
+code --install-extension castwide.solargraph
+code --install-extension misogi.ruby-rubocop
+code --install-extension cssho.vscode-svgviewer
+code --install-extension formulahendry.terminal
+code --install-extension gruntfuggly.todo-tree
+code --install-extension vscodevim.vim
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-python.python
+code --install-extension hbenl.vscode-test-explorer
+code --install-extension waderyan.nodejs-extension-pack
+code --install-extension rebornix.ruby
