73d5016ce4c87c1ea1c069a827230002ef8573b7
Josh Young <j22cal@gmail.com>
2021-03-29 16:03:13 -0500

    Setup symbolic links

--- copy_config.sh
+++ copy_config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source "./env.sh"
 source "./utility_functions.sh"
 
 current_dir=`pwd`
@@ -41,3 +42,24 @@ cp "$dir/init.vim" "$neovim_path/init.vim"
 backup_config ".ctags"
 cat $dir/.ctags > $dir/default.ctags
 cp "$dir/default.ctags" "$ctags_path/default.ctags"
+
+if ! [[ -z "$GITHUB_DIRECTORY_SOURCE" ]] || ! [[ -z "$GITHUB_DIRECTORY_DESTINATION" ]]; then
+  if ! [[ -L $GITHUB_DIRECTORY_DESTINATION ]] && ! [[ -e $GITHUB_DIRECTORY_DESTINATION ]]; then
+    echo "Created Symbolic Link"
+    ln -s $GITHUB_DIRECTORY_SOURCE $GITHUB_DIRECTORY_DESTINATION
+  fi
+fi
+
+if ! [[ -z "$GITLAB_DIRECTORY_SOURCE" ]] || ! [[ -z "$GITLAB_DIRECTORY_DESTINATION" ]]; then
+  if ! [[ -L $GITLAB_DIRECTORY_DESTINATION ]] && ! [[ -e $GITLAB_DIRECTORY_DESTINATION ]]; then
+    echo "Created Symbolic Link"
+    ln -s $GITLAB_DIRECTORY_SOURCE $GITLAB_DIRECTORY_DESTINATION
+  fi
+fi
+
+if ! [[ -z "$DOCKER_DIRECTORY_SOURCE" ]] || ! [[ -z "$DOCKER_DIRECTORY_DESTINATION" ]]; then
+  if ! [[ -L $DOCKER_DIRECTORY_DESTINATION ]] && ! [[ -e $DOCKER_DIRECTORY_DESTINATION ]]; then
+    echo "Created Symbolic Link"
+    ln -s $DOCKER_DIRECTORY_SOURCE $DOCKER_DIRECTORY_DESTINATION
+  fi
+fi
